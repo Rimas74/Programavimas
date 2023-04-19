2119a08e22a9064d309ed452c286829fdd1debba
rimantas gintilas <gintilas.rimantas@gmail.com>
2023-04-19 17:14:31 +0300

    uzduotis 28

---
+++ Uzduotis_10/uzduotis_28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduotis_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsvestiTeksta("Sveikas pasauli!");
+
+            Console.ReadKey();
+        }
+
+        static void IsvestiTeksta(string tekstas)
+        {
+            Console.WriteLine(tekstas);
+        }
+    }
+}
