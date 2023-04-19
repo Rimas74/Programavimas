180da890d4427dc924a35d2e253a70735d1e396c
rimantas gintilas <gintilas.rimantas@gmail.com>
2023-04-19 18:02:41 +0300

    uzduotis 30

---
+++ Uzduotis_10/uzduotis_30/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduotis_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leiskite vartotojui įvesti du sveikuosius skaičius į programą.  
+             * Sukurkite ir panaudokite šiuos metodus:
+                Suma;
+                Skirtumas;
+                Sandauga;
+                Dalmuo;
+                */
+            Console.WriteLine("įveskit pirmą sveiką skaičių:");
+            int skaiciusPirmas = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("įveskit antrą sveiką skaičių:");                      
+            int skaiciusAntras = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{skaiciusPirmas} + {skaiciusAntras} = {Suma(skaiciusPirmas, skaiciusAntras)}");
+            Console.WriteLine($"{skaiciusPirmas} - {skaiciusAntras} = {Skirtumas(skaiciusPirmas, skaiciusAntras)}");
+            Console.WriteLine($"{skaiciusPirmas} * {skaiciusAntras} = {Sandauga(skaiciusPirmas, skaiciusAntras)}");
+            Console.WriteLine($"{skaiciusPirmas} / {skaiciusAntras} = {Dalyba(skaiciusPirmas, skaiciusAntras):N2}");
+
+        }
+
+        private static double Dalyba(int skaiciusPirmas, int skaiciusAntras)
+        {
+            return (double )skaiciusPirmas / skaiciusAntras;
+        }
+
+        private static object Sandauga(int skaiciusPirmas, int skaiciusAntras)
+        {
+            return skaiciusPirmas * skaiciusAntras;
+        }
+
+        private static int Skirtumas(int skaiciusPirmas, int skaiciusAntras)
+        {
+            return skaiciusPirmas - skaiciusAntras;
+        }
+
+        private static int Suma(int skaiciusPirmas, int skaiciusAntras)
+        {
+            return skaiciusPirmas + skaiciusAntras;
+        }
+
+
+
+    }
+}
