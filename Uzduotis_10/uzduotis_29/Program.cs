fd315a9c6b35e2ca5450d7c9c0a8336a31aa4625
rimantas gintilas <gintilas.rimantas@gmail.com>
2023-04-19 17:29:18 +0300

    uzduotis 29

---
+++ Uzduotis_10/uzduotis_29/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduotis_29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Parašykite metodą, kuris pasisveikintų su vartotoju.
+            Vartotojas turi įvesti savo vardą, amžių ir hobio pavadinimą.
+            Visi šie duomenys yra duodami metodui, kuris visą šią informaciją išveda
+            formatuotai
+            */
+            Pasisveikinti();
+        }
+        public static void Pasisveikinti()
+        {
+            Console.Write("Įveskite savo vardą:\n");
+            string vardas = Console.ReadLine();
+
+            Console.Write("Įveskite savo amžių:\n");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Įveskite savo hobio pavadinimą:\n");
+            string hobis = Console.ReadLine();
+
+            Console.WriteLine($"Labas, {vardas} ({amzius} m.).\nJūsų įrašytas hobis: {hobis}.");
+            
+        }
+    }
+}
